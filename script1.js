c4a0bd62cc66f3161a6893216a95d5eb8b5f9108
Baranijeni <120646632+Baranijeni@users.noreply.github.com>
2023-01-02 11:13:08 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,52 @@
+var head1=[ 
+    {
+        
+           careerobjective : "to be a part o fo company that indulges professional growth which provides challenging and rewaarding career whilw allowing me to utilize my knowledge and skills.",
+    },
+    {
+    summary :"i am good fun and friendly person, i am honest and punctual. i ahve good communication skill. i have creative mind and am always realy for new challenges. i can deal with difficulti situations. maybe a lot of people are more the good then me, but with perseverance and strong will can defeat all."
+    },
+    {
+        personalskills:"vreativity, interpersonal skills, critical thinking, problem solving, public speaking, customer service skills, teamwork skill, communication."
+    }
+    ]
+     var second =[
+    {
+        Qualification:'Bsc., physics',
+        Schoolcollege:'bon secours college for women',
+        Board:'Bharathidasan university',
+        yearofpassing:'2020',
+        percentageCGPA:'65%'
+    },
+    {
+        Qualification:'HSC',
+        Schoolcollege:'prphs piblic hr.sec school',
+        Board:'State board',
+        yearofpassing:'2017',
+        percentageCGPA:'56%'
+    },
+    {
+        Qualification:'SSLC',
+        Schoolcollege:'prphs public hr.sec school',
+        Board:'State board',
+        yearofpassing:'2015',
+        percentageCGPA:'83%'
+    }
+     ]
+    
+     var skills=
+        ["c","c++","java","HTML5", "CSS3", "JAVASCRIPT", "RDBMS,", "SQL", "ms office"]
+        var personalinfo={
+    name:"R.Barani",
+    Father:"G.rajendhran",
+    DOB:"25.04.2000",
+    Gender:"male",
+    Materialstatus:"single",
+    Nationality:"indian",
+    Religion:"hindu",
+    Mobileno: 6383318726
+        }
+    console.log(head1)
+    console.table(second)
+    console.log(skills)
+    console.table(personalinfo)
